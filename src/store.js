08d5f97a118226a47c573abad760443ee85d462d
aslanyi <mustafatahatepedelen@gmail.com>
2020-05-18 12:19:27 +0300

    redux-persist was incl.

--- src/store.js
+++ src/store.js
@@ -2,10 +2,20 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './redux/reducers';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import storage from 'redux-persist/lib/storage';
 import { firebaseInit, FirebaseHelper, getFirestore, getAuth } from '../firebase';
+import { persistReducer } from 'redux-persist';
+
+const persistConfig = {
+    key: 'primary',
+    storage: storage,
+    whiteList: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const makeStore = (initialState = {}) => {
     firebaseInit();
     FirebaseHelper.singleton.getInstance(getFirestore(), getAuth());
-    return createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
+    return createStore(persistedReducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
 };
