19efd8fa2fe18fd2ba1ee6512c25106d4c99b42b
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-06-02 11:15:42 +0300

    global style was added for storybook

--- .storybook/themeDecorator.js
+++ .storybook/themeDecorator.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Theme from '../src/utils/theme';
+import GlobalStyle from '../src/utils/globalStyle';
 
-const ThemeDecorator = (storyFn) => <ThemeProvider theme={Theme}>{storyFn()}</ThemeProvider>;
+const ThemeDecorator = (storyFn) => (
+    <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        {storyFn()}
+    </ThemeProvider>
+);
 
 export default ThemeDecorator;
